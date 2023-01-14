fe89273db45e149ab381d4699756afe95fb31662
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-14 21:12:09 +0200

    ADD starter & main SCSS files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import "./scss/main.module.scss";
+
 import App from './App';
 
 const main = ReactDOM.createRoot(document.getElementById('main'));
