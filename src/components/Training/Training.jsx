02f7fd3ebc0df61d1bb277f89715862d96d8b67a
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-15 18:10:19 +0200

    ADD Training component content & EDIT MainButton

--- src/components/Training/Training.jsx
+++ src/components/Training/Training.jsx
@@ -1,8 +1,41 @@
+import { MainButton, TopComponent } from "../index";
+
+import styles from "./_Training.module.scss";
+
+import Image1 from "../../assets/images/moon.jpg";
+import Image2 from "../../assets/images/pod.jpg";
+import { useEffect } from "react";
+
 const Training = () => {
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
-        <div>
-            Training
-        </div>
+        <section className={styles.training}>
+            <TopComponent title="training" description="pre-flight & in-flight training." />
+
+            <div className={styles.details}>
+                <div className={styles.first}>
+                    <h4 className={styles["first-title"]}>training</h4>
+
+                    <p className={styles["first-description"]}>Thorough training is a necessity when traveling to space.
+                        We offer all inclusive training for pre-flight and in-flight scenarios.</p>
+
+                    <MainButton linkTo={"/contact"} text={"contact"} />
+                </div>
+
+                <div className={styles.second}>
+                    <div className={styles["second-img1"]}>
+                        <img src={Image1} alt="Moon" draggable="false" />
+                    </div>
+
+                    <div className={styles["second-img2"]}>
+                        <img src={Image2} alt="Pod" draggable="false" />
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
